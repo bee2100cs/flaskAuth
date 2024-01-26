5744009bae810ce391b57f9bf861b68c24d51938
Bryan Muchai <bee2100cs@gmail.com>
2024-01-26 21:18:39 +0000

    first commit

---
+++ flask/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, Blueprint, session, render_template, request, redirect
+import pyrebase
+
+bp = Blueprint("main", __name__)
+
+bp.route("/")
+def index():
+
+    return render_template('home.html')
+
+
